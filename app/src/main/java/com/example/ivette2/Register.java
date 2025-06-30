e7c5a010e5a31673d81b0e665cb4fcdb6db17949
Daniel Flores Zaragoza <dafloresza@gmail.com>
2025-06-29 19:34:20 -0600

    mi proyecto

---
+++ app/src/main/java/com/example/ivette2/Register.java
@@ -0,0 +1,75 @@
+package com.example.ivette2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Register extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            mAuth = FirebaseAuth.getInstance();
+            return insets;
+        });
+    }
+
+    public void Añadir_usuario(View view) {
+        EditText email, pass1, pass2;
+
+        email = findViewById(R.id.correo);
+        pass1 = findViewById(R.id.pass1);
+        pass2 = findViewById(R.id.pass2);
+
+        String temail = email.getText().toString().trim();
+        String tpass1 = pass1.getText().toString().trim();
+        String tpass2 = pass2.getText().toString().trim();
+
+        if (pass1.getText().toString() == pass2.getText().toString()) {
+            Toast.makeText(this, "la contraseña no coincide", Toast.LENGTH_SHORT).show();
+            pass1.setText("");
+            pass2.setText("");
+        }
+        else {
+            Toast.makeText(this, "la contraseña no coincide", Toast.LENGTH_SHORT).show();
+            pass1.setText("");
+            pass2.setText("");
+        }
+    }
+
+    public void Registrar(String email, String password) {
+        Toast.makeText(this, "Registrando...", Toast.LENGTH_SHORT).show();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            finish();
+                        }
+                    }
+                });
+    }
+}
